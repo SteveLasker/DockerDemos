65c8e0882d9a463493b93f0c1586f826c7b77577
Steve Lasker <StevenLasker@Hotmail.com>
2015-10-21 17:16:46 -0700

    Variabalized the script
    Changed all named instance to variables

--- Go_WebServerHelloWorld/dockerBuildRun.sh
+++ Go_WebServerHelloWorld/dockerBuildRun.sh
@@ -1,10 +1,16 @@
+# Parameters
+imageName="go_basic"
+externalPort=8000
+containerPort=8080
+containerName=go_web_server
+
 # Clear out any running containers and images in place for our new build
-docker kill $(docker ps -a -q -f "image=go_basic")
-docker rm $(docker ps -a -q -f "image=go_basic")
-docker rmi go_basic
+docker kill $(docker ps -a -q -f "image=$imageName")
+docker rm $(docker ps -a -q -f "image=$imageName")
+docker rmi $imageName
 # Build the new container image
-docker build -t go_basic .
+docker build -t $imageName .
 # Run it on the default port, with a target name
-docker run -p 8000:8080 -di --name=go_web_server go_basic
+docker run -p $externalPort:$containerPort -di --name=$containerName $imageName
 # Launch the page, assuming it's a web app 
-open http://192.168.99.100:8000/
+open http://$(docker-machine ip default):$externalPort
