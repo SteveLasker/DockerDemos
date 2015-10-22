4d0b28e5495443e7cff2c193fce9d91984c28c61
Steve Lasker <StevenLasker@Hotmail.com>
2015-10-21 20:21:23 -0700

    Script Refactoring
    Updated the docker build/run scripts to be parameterized to build, run
    or both
    Added the beginning of a content file

--- Go_WebServerHelloWorld/Scripts/dockerBuildRun.sh
+++ Go_WebServerHelloWorld/Scripts/dockerBuildRun.sh
@@ -3,15 +3,43 @@ imageName="go_basic"
 externalPort=8000
 containerPort=8080
 containerName=go_web_server
+buildRunOption=${1}
 
-# Clear out any running containers and images in place for our new build
-docker kill $(docker ps -a -q -f "image=$imageName")
-docker rm $(docker ps -a -q -f "image=$imageName")
-docker rmi $imageName
-# Build the new container image
-echo Build
-docker build -t $imageName ../src/
-# Run it on the default port, with a target name
-docker run -p $externalPort:$containerPort -di --name=$containerName $imageName
-# Launch the page, assuming it's a web app 
-open http://$(docker-machine ip default):$externalPort
+if [ $buildRunOption == "" ]
+then
+	buildRunOption = "BuildRun"
+fi
+
+echo "Docker Build/Run Options: " $buildRunOption 
+
+ImageBuild () {
+	echo "Building the Image"
+	# Clear out any running containers and images in place for our new build
+	docker kill $(docker ps -a -q -f "image=$imageName")
+	docker rm $(docker ps -a -q -f "image=$imageName")
+	docker rmi $imageName
+	# Build the new container image
+	echo Build
+	docker build -t $imageName ../src/
+}
+
+ImageRun () {
+	echo "Running the Image"
+	# Run it on the default port, with a target name
+	docker run -p $externalPort:$containerPort -di --name=$containerName $imageName
+	# Launch the page, assuming it's a web app 
+	open http://$(docker-machine ip default):$externalPort
+}
+
+case "$buildRunOption" in
+	"Build")
+		ImageBuild
+		;;
+	"Run")
+		ImageRun
+		;;
+	"BuildRun")
+		ImageBuild
+		ImageRun
+		;;
+esac
--- Go_WebServerHelloWorld/src/WebServerHello.go
+++ Go_WebServerHelloWorld/src/WebServerHello.go
@@ -7,11 +7,10 @@ import(
 )
  
 func indexHandler( w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello World, I'm running on %s with an %s CPU ", runtime.GOOS,runtime.GOARCH)
- 
+	fmt.Fprintf(w, "Hello World, I'm running on %s with an %s CPU ", runtime.GOOS, runtime.GOARCH)
 }
  
 func main(){
 	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8080",nil)
+	http.ListenAndServe(":8080", nil)
 }
