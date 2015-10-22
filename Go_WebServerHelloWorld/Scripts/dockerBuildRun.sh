46a81e0d27b358b0f5f524a12bbf3f2854b5600d
Steve Lasker <StevenLasker@Hotmail.com>
2015-10-21 17:27:33 -0700

    Restructured Folders

--- Go_WebServerHelloWorld/Scripts/dockerBuildRun.sh
+++ Go_WebServerHelloWorld/Scripts/dockerBuildRun.sh
@@ -9,7 +9,8 @@ docker kill $(docker ps -a -q -f "image=$imageName")
 docker rm $(docker ps -a -q -f "image=$imageName")
 docker rmi $imageName
 # Build the new container image
-docker build -t $imageName .
+echo Build
+docker build -t $imageName ../src/
 # Run it on the default port, with a target name
 docker run -p $externalPort:$containerPort -di --name=$containerName $imageName
 # Launch the page, assuming it's a web app 
